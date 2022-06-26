179d5870593ad950c75b3ab3e6aea0f3092f4e69
YouKyoung-Na <dbrudskql823@naver.com>
2022-06-26 21:23:57 +0900

    2022.06.26 Mac

---
+++ 백준2525_오븐시계.py
@@ -0,0 +1,13 @@
+hour, minute = map(int, input().split())
+time = int(input())
+
+minute += time
+
+if(minute>=60):
+    hour += int(minute/60)
+    minute %= 60    
+
+if hour >= 24:
+    hour -= 24
+
+print(hour, minute)
