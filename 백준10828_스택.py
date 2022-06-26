7ebc7957549b004315911de3aa33f5495e4c7ee1
YouKyoung-Na <dbrudskql823@naver.com>
2022-06-27 04:46:36 +0900

    2022.06.27 Mac

---
+++ 백준10828_스택.py
@@ -0,0 +1,23 @@
+import sys
+n= int(input())
+stack = []
+
+for _ in range(n):  
+    com = sys.stdin.readline().split()
+    if(com[0] == 'push'):
+        stack.append(com[1])
+    elif(com[0]=='pop'):
+        if(len(stack)==0):
+            print('-1')
+        else:
+            print(stack.pop())
+    elif(com[0]=='size'):
+        print(len(stack))
+    elif(com[0]=='empty'):
+        if(len(stack)==0): print(1)
+        else : print(0)
+    elif(com[0]=='top'):
+        if (len(stack)==0):
+            print('-1')
+        else:
+            print(stack[-1])
---
+++ 백준11720_숫자의합.py
@@ -0,0 +1,12 @@
+n = int(input())
+num = int(input())
+nums = [int(i) for i in str(num)]
+print(sum(nums))
+
+# 정석
+# a = int(input())
+# n = list(input())
+# sum = 0
+# for i in n:
+#     sum += int(i)
+# print(sum)
