5b77bd3ab9a37440fbcad9cfedca9a79d6387126
YouKyoung-Na <dbrudskql823@naver.com>
2022-06-28 16:04:18 +0900

    2022.06.28 Mac

---
+++ 백준1436_영화감독숌.py
@@ -0,0 +1,5 @@
+n= int(input())
+if(n>0):
+    print(str(n-1)+'666')
+else:
+    print('666')
---
+++ 전역일달력.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+import sys
+# 육군 545일
+time1=int(input("입대 년도,월,일을 공백없이 8칸 작성하시오: "))
+time2 = time1
+print(time1)
